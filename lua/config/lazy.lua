38c430ef6d1dd22e8ecdc88cffbcedaf9d29a2d7
yushiyuan <yushiyuan@didiglobal.com>
2023-10-24 19:49:06 +0800

    use lazyvim to manage extra plugin

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -10,28 +10,6 @@ require("lazy").setup({
 	spec = {
 		-- add LazyVim and import its plugins
 		{ "LazyVim/LazyVim", import = "lazyvim.plugins" },
-		-- import any extras modules here
-		-- { import = "lazyvim.plugins.extras.lang.typescript" },
-		-- { import = "lazyvim.plugins.extras.lang.json" },
-		-- { import = "lazyvim.plugins.extras.ui.mini-animate" },
-		-- import/override with your plugins
-		{ import = "lazyvim.plugins.extras.ui.mini-animate" },
-		{ import = "lazyvim.plugins.extras.ui.edgy" },
-		{ import = "lazyvim.plugins.extras.editor.navic" },
-		{ import = "lazyvim.plugins.extras.editor.mini-files" },
-		{ import = "lazyvim.plugins.extras.editor.aerial" },
-		{ import = "lazyvim.plugins.extras.coding" },
-		{ import = "lazyvim.plugins.extras.dap" },
-		{ import = "lazyvim.plugins.extras.formatting" },
-		{ import = "lazyvim.plugins.extras.lang.clangd" },
-		{ import = "lazyvim.plugins.extras.lang.cmake" },
-		{ import = "lazyvim.plugins.extras.lang.go" },
-		{ import = "lazyvim.plugins.extras.lang.json" },
-		{ import = "lazyvim.plugins.extras.lang.python" },
-		{ import = "lazyvim.plugins.extras.lang.yaml" },
-		{ import = "lazyvim.plugins.extras.linting" },
-		{ import = "lazyvim.plugins.extras.test" },
-		{ import = "lazyvim.plugins.extras.util" },
 		{ import = "plugins" },
 	},
 	defaults = {
