b0c7e47a80fd48ed05c81de3533217f8b1b014fc
yshiyuan <shiyuan.yu@outlook.com>
2025-09-18 13:52:47 +0800

    switch autosave on in extend-conform.lua

--- lua/plugins/extend-conform.lua
+++ lua/plugins/extend-conform.lua
@@ -10,5 +10,10 @@ return {
 				html = { "prettier" },
 			},
 		},
+		-- 配置保存时自动格式化
+		format_on_save = {
+			timeout_ms = 500, -- 超时时间
+			lsp_fallback = true, -- 如果 conform 失败，尝试使用 LSP 格式化
+		},
 	},
 }
