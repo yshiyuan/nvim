19a30c5713f87989b2be003bf5ed2cf57461013d
yshiyuan <shiyuan.yu@outlook.com>
2025-09-29 13:49:55 +0800

    add keymap for paste and copy in neovide

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+if vim.g.neovide then
+	vim.api.nvim_set_keymap("v", "<sc-c>", '"+y', { noremap = true })
+	vim.api.nvim_set_keymap("n", "<sc-v>", 'l"+P', { noremap = true })
+	vim.api.nvim_set_keymap("v", "<sc-v>", '"+P', { noremap = true })
+	vim.api.nvim_set_keymap("c", "<sc-v>", '<C-o>l<C-o>"+<C-o>P<C-o>l', { noremap = true })
+	vim.api.nvim_set_keymap("i", "<sc-v>", '<ESC>l"+Pli', { noremap = true })
+	vim.api.nvim_set_keymap("t", "<sc-v>", '<C-\\><C-n>"+Pi', { noremap = true })
+end
