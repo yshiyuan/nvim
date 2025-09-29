2bca7c471cea0f7bdab5a0c91ead8dca06c3a9bb
yshiyuan <shiyuan.yu@outlook.com>
2025-09-29 14:22:47 +0800

    config mini files

--- lua/plugins/extend-mini-files.lua
+++ lua/plugins/extend-mini-files.lua
@@ -1,49 +1,58 @@
 return {
-	"echasnovski/mini.files",
-	enabled = true,
-	keys = {
-		{
-			"<leader>e",
-			function()
-				require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
-			end,
-			desc = "Open mini.files (directory of current file)",
+	{
+		"nvim-mini/mini.files",
+		enabled = true,
+		lazy = false,
+		keys = {
+			{
+				"<leader>e",
+				function()
+					require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+				end,
+				desc = "Open mini.files (directory of current file)",
+			},
+			{
+				"<leader>E",
+				function()
+					require("mini.files").open(vim.loop.cwd(), true)
+				end,
+				desc = "Open mini.files (cwd)",
+			},
+			{
+				"<leader>fm",
+				function()
+					require("mini.files").open(LazyVim.root(), true)
+				end,
+				desc = "Open mini.files (root)",
+			},
 		},
-		{
-			"<leader>E",
-			function()
-				require("mini.files").open(vim.loop.cwd(), true)
-			end,
-			desc = "Open mini.files (cwd)",
-		},
-		{
-			"<leader>fm",
-			function()
-				require("mini.files").open(LazyVim.root(), true)
-			end,
-			desc = "Open mini.files (root)",
+		opts = {
+			mappings = {
+				go_in = "<S-L>",
+				go_out = "<S-H>",
+				go_in_plus = "<S-Right>",
+				go_out_plus = "<S-Left>",
+				go_in_vertical = "<C-V>",
+				go_in_horizontal = "<C-S>",
+				go_in_vertical_plus = "<C-V>",
+				go_in_horizontal_plus = "<C-S>",
+				toggle_hidden = ";.",
+				change_cwd = ";c",
+			},
+			windows = {
+				width_nofocus = 20,
+				width_focus = 50,
+				width_preview = 100,
+			},
+			options = {
+				use_as_default_explorer = true,
+			},
 		},
 	},
-	opts = {
-		mappings = {
-			go_in = "<S-L>",
-			go_out = "<S-H>",
-			go_in_plus = "<S-Right>",
-			go_out_plus = "<S-Left>",
-			go_in_vertical = "<C-V>",
-			go_in_horizontal = "<C-S>",
-			go_in_vertical_plus = "<C-V>",
-			go_in_horizontal_plus = "<C-S>",
-			toggle_hidden = ";.",
-			change_cwd = ";c",
-		},
-		windows = {
-			width_nofocus = 20,
-			width_focus = 50,
-			width_preview = 100,
-		},
-		options = {
-			use_as_default_explorer = true,
+	{
+		"folke/snacks.nvim",
+		opts = {
+			explorer = { enabled = false },
 		},
 	},
 }
