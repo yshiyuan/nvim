379cd11a2f1019f7d8a2705dcfa7178ff2ed6862
yshiyuan <shiyuan.yu@outlook.com>
2025-11-18 11:08:23 +0800

    set expandtab

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,10 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.bo.tabstop = 4 -- size of a hard tabstop (ts).
-vim.bo.shiftwidth = 4 -- size of an indentation (sw).
-vim.bo.expandtab = true -- always uses spaces instead of tab characters (et).
-vim.bo.softtabstop = 4 -- number of spaces a <Tab> counts for. When 0, feature is off (sts).
+vim.o.tabstop = 4 -- size of a hard tabstop (ts).
+vim.o.shiftwidth = 4 -- size of an indentation (sw).
+vim.o.expandtab = true -- always uses spaces instead of tab characters (et).
+vim.o.softtabstop = 4 -- number of spaces a <Tab> counts for. When 0, feature is off (sts).
 
 local current_path = vim.fn.getcwd()
 vim.g.python3_host_prog = current_path .. "/venv/bin/python"
