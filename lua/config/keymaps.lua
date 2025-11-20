c716fe6803d64faa1819ea91455babf420b1fe8b
yshiyuan <shiyuan.yu@outlook.com>
2025-11-20 12:04:16 +0800

    just vscode config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,9 +1,3 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-
-if vim.g.neovide then
-	vim.keymap.set({ "n", "v", "s", "x", "o", "i", "l", "c", "t" }, "<C-S-v>", function()
-		vim.api.nvim_paste(vim.fn.getreg("+"), true, -1)
-	end, { noremap = true, silent = true })
-end
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,13 +5,3 @@ vim.o.tabstop = 4 -- size of a hard tabstop (ts).
 vim.o.shiftwidth = 4 -- size of an indentation (sw).
 vim.o.expandtab = true -- always uses spaces instead of tab characters (et).
 vim.o.softtabstop = 4 -- number of spaces a <Tab> counts for. When 0, feature is off (sts).
-
-local current_path = vim.fn.getcwd()
-vim.g.python3_host_prog = current_path .. "/venv/bin/python"
-
-vim.opt.clipboard:append("unnamedplus")
-
-vim.g.autoformat = false
-
--- neovide config
-vim.g.neovide_fullscreen = true
