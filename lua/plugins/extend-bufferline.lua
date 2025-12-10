69168667921ad76cf017f3c0cef26eece1420cd1
yshiyuan <shiyuan.yu@outlook.com>
2025-12-10 11:54:30 +0800

    add buffline custom config

---
+++ lua/plugins/extend-bufferline.lua
@@ -0,0 +1,15 @@
+return {
+	{
+		"akinsho/bufferline.nvim",
+		opts = {
+			options = {
+				indicator = {
+					style = "underline",
+				},
+				separator_style = "slope",
+				always_show_bufferline = true,
+				show_buffer_close_icons = false,
+			},
+		},
+	},
+}
